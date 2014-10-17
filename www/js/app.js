f4b73560df64708b434b75e7b14d696a2f655932
Ola Ekstrand <ola.ekstrand@gridprosoftware.com>
2014-10-17 08:07:21 +0200

    Changed default page

--- www/js/app.js
+++ www/js/app.js
@@ -83,7 +83,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/login');
+  $urlRouterProvider.otherwise('/login');
 
 });
 
