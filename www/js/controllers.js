803db1fbb0d0c9a032e159d9ae6aaa59bce92416
Ola Ekstrand <ola.ekstrand@gridprosoftware.com>
2014-10-21 19:54:43 +0200

    tes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,4 @@
 angular.module('starter.controllers', [])
-.controller('DashCtrl', function ($scope) {
-})
-
-.controller('FriendsCtrl', function ($scope, Friends) {
-    $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
-    $scope.friend = Friends.get($stateParams.friendId);
-})
 
 .controller("ViewsCtrl", ['$http', '$scope', '$state', 'viewService', function ($http, $scope, $state, viewService) {
     $scope.views = [];
